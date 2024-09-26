4b376bc74b2577d066adc5ff102c7fe75cca3a4e
Eduardabrasilc <maria.costa@sptech.school>
2024-09-26 14:17:40 -0300

    headerGerenciamento

---
+++ senhor-sorvete/src/Components/HeaderGerenciamento/index.jsx
@@ -0,0 +1,17 @@
+import "./headerGerenciamento.css";
+
+
+const headerGerenciamento = () => {
+
+  return (
+                <div className="header-home">
+    <div className="containerImgLogoAmarelo">
+                <img src="/Imagens/logoAmarelo-removebg-preview.png" alt="" />
+            </div>
+      <h4 className="titulo">Mestre Sorvete</h4>
+    </div>
+  
+  );
+
+}  
+export default headerGerenciamento;
---
+++ senhor-sorvete/src/Pages/HomeGerenciamento/index.jsx
@@ -0,0 +1,14 @@
+
+import HeaderGerencimento from'../../Components/HeaderGerenciamento';
+
+const HomeGerenciamento = () => {
+
+    return (
+        <div>
+           <HeaderGerencimento/>
+        </div>
+    );
+
+}
+
+export default HomeGerenciamento;
