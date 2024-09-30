4e95c2c36d487d21e1abd2bc3ca0ca19baace641
gui-oliveiraa <guilherme.oliveira@sptech.school>
2024-09-30 02:15:44 -0300

    Seguindo desenvolvimento da homepage

---
+++ senhor-sorvete/src/Components/IconeWhatsapp/index.jsx
@@ -0,0 +1,18 @@
+import "./iconeWhatsapp.css";
+
+const IconeWhatsapp = (props) => {
+  return (
+    <div className="whatsapp-icon">
+      <a
+        href="https://wa.me/00000000000"
+        target="_blank"
+        className="whatsapp-icon"
+        rel="noopener noreferrer"
+      >
+        <img src="Imagens/logo-whatsapp.png" alt="WhatsApp Icon" />
+      </a>
+    </div>
+  );
+};
+
+export default IconeWhatsapp;
--- senhor-sorvete/src/Pages/Home/index.jsx
+++ senhor-sorvete/src/Pages/Home/index.jsx
@@ -1,21 +1,14 @@
 import "./home.css";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
+import IconeWhatsapp from "../../Components/IconeWhatsapp";
 
 const Home = (props) => {
   return (
     <div className="home">
       <Header />
       <main>
-        <a
-          href="https://wa.me/00000000000"
-          target="_blank"
-          className="whatsapp-icon"
-          rel="noopener noreferrer"
-        >
-          <img src="Imagens/logo-whatsapp.png" alt="WhatsApp Icon" />
-        </a>
-
+        <IconeWhatsapp />
         <section className="hero">
           <div className="content">
             <h4>Bem vindo à</h4>
@@ -30,53 +23,66 @@ const Home = (props) => {
               <button className="btn-secondary">Entrar em contato</button>
             </div>
           </div>
+          <img src="Imagens/imagem-principal-homepage.png" alt="" />
         </section>
 
         <section className="sobre-nos">
           <div className="sobre-container">
-            <div className="sobre-text">
-              <h2>Sobre nós</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac
-                vestibulum risus. Phasellus quis neque non nunc malesuada
-                fringilla.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                efficitur libero at lacus fermentum, non viverra mi viverra.
-              </p>
-            </div>
             <div className="sobre-img">
-              <img src="" alt="Imagem sobre nós" />
+              <img
+                src="Imagens/sorvete-doce-de-leite.png"
+                alt="Imagem sobre nós"
+              />
+            </div>
+            <div className="sobre-text">
+              <h2>——— Sobre nós ———</h2>
+              <p>
+                Somos apaixonados por oferecer os melhores sorvetes do mercado,
+                selecionando cuidadosamente para revenda em nossa loja. Com um
+                compromisso com a qualidade, trazemos para você uma variedade de
+                sabores irresistíveis.
+              </p>
+              <p>
+                Nosso objetivo é proporcionar uma experiência refrescante e
+                deliciosa em cada visita, oferecendo sorvetes que conquistam
+                todos os paladares. Visite-nos e descubra sua próxima sobremesa
+                favorita!
+              </p>
             </div>
           </div>
         </section>
 
         <section className="populares">
-          <h1>Populares!</h1>
+          <h1>——— Populares! ———</h1>
           <div className="carousel">
             <div className="carousel-inner">
               <div className="card">
+                <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Banana Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
               <div className="card">
+              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Chocolate Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
               <div className="card">
+              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Morango Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
               <div className="card">
+              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Baunilha Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
               <div className="card">
+              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Café Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
               <div className="card">
+              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
                 <h3>Caramelo Soft Cream</h3>
                 <p>R$ 9,99</p>
               </div>
@@ -84,53 +90,49 @@ const Home = (props) => {
             <button className="carousel-prev">&lt;</button>
             <button className="carousel-next">&gt;</button>
           </div>
-          <a href="#" className="ver-tudo">
-            Ver tudo
-          </a>
+          <div className="ver-tudo">
+            <a href="/">ver tudo</a>
+          </div>
         </section>
 
+
         <section className="sugestao-do-dia">
-          <div className="sugestao-container">
-            <div className="sugestao-text">
-              <h2>Sugestão do Dia</h2>
+        {/* <h2>Sugestão do Dia</h2>
               <p className="data">
                 <span id="data-atual"></span>
-              </p>
+              </p> */
+              /* <div class name="sugestao-container"> </div> */}
+          <div className="sugestao-container">
+            <div className="sugestao-img">
+              <img src="Imagens/imagem-3-homepage.png" alt="Sugestão do Dia - Chocolate Trufado" />
+            </div>
+            <div className="sugestao-text">
               <h3 className="titulo-sugestao">Chocolate trufado</h3>
               <p className="descricao">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                id felis eu est sodales tempor. Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit. Duis ac vestibulum risus.
+              Hoje, nossa dica especial é o irresistível sorvete de Chocolate Trufado. Com uma combinação perfeita de cremosidade e sabor intenso. Venha experimentar essa delícia que derrete na boca e transforma o seu dia em um momento de puro prazer!
               </p>
               <button className="btn-experimentar">Experimentar</button>
+              </div>
             </div>
-            <div className="sugestao-img">
-              <img src="" alt="Sugestão do Dia - Chocolate Trufado" />
-            </div>
-          </div>
         </section>
 
         <section className="more-info">
           <div className="more-text">
-            <h1>Mais!</h1>
+            <h1>——— Mais! ———</h1>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-              lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-              malesuada. Aenean vehicula velit nec tellus tempus ullamcorper.
+            Mergulhe na arte da culinária gastronômica com um toque refrescante! Descubra como os mestres da cozinha transformam ingredientes simples em verdadeiras obras de arte geladas. Acompanhe nossos vídeos inspiradores, onde exploramos técnicas sofisticadas de preparo de sorvetes e picolés, desde a escolha dos ingredientes até a apresentação final.
             </p>
             <button className="btn-primary">Saiba Mais</button>
           </div>
           <div className="more-img">
-            <img src="" alt="Imagem placeholder" />
+            <img src="Imagens/sorvete-chocolate-plantas.png" alt="Imagem placeholder" />
           </div>
         </section>
 
         <section className="newsletter">
           <h2>Receber notificações</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-            quis sem at nibh elementum imperdiet.
+          Não perca a chance de ser o primeiro a saber quando novos sabores chegam à nossa sorveteria! Inscreva-se para receber notificações por e-mail e fique por dentro de todas as novidades.
           </p>
           <form className="newsletter-form">
             <input type="email" placeholder="Email" required />
