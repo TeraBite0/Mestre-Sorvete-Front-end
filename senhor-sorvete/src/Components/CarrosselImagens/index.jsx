64fad5c5e3de1876ca1eded9415758fab70db278
gui-oliveiraa <guilherme.oliveira@sptech.school>
2024-10-07 16:29:47 -0300

    Inserindo script do carrossel de imagens

---
+++ senhor-sorvete/src/Components/CarrosselImagens/index.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./carrosselImagens.css";
+
+const CarrosselImagens = () => {
+  const carouselInnerRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalCards = 6; // Total de cards no carrossel
+  const visibleCards = 3; // Número de cards visíveis ao mesmo tempo
+
+  // Mover o carrossel
+  const updateCarousel = () => {
+    const cardWidth =
+      carouselInnerRef.current.querySelector(".card").offsetWidth;
+    const newTransformValue = -currentIndex * (cardWidth + 20); // margin - 20px
+    carouselInnerRef.current.style.transform = `translateX(${newTransformValue}px)`;
+  };
+
+  // Avançar o carrossel
+  const handleNextClick = () => {
+    if (currentIndex < totalCards - visibleCards) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0); // Volta ao início quando chega no final
+    }
+  };
+
+  // Retroceder o carrossel
+  const handlePrevClick = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(totalCards - visibleCards); // Vai para o final
+    }
+  };
+
+  useEffect(() => {
+    updateCarousel(); // Atualiza o carrossel sempre que o currentIndex mudar
+  }, [currentIndex]);
+
+  useEffect(() => {
+    let autoScroll = setInterval(() => {
+      if (currentIndex < totalCards - visibleCards) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(0);
+      }
+    }, 3000);
+
+    const carouselInner = carouselInnerRef.current;
+
+    // Pausando o auto-scroll quando o mouse estiver sobre o carrossel
+    const handleMouseOver = () => clearInterval(autoScroll);
+    const handleMouseOut = () =>
+      (autoScroll = setInterval(() => {
+        if (currentIndex < totalCards - visibleCards) {
+          setCurrentIndex(currentIndex + 1);
+        } else {
+          setCurrentIndex(0);
+        }
+      }, 3000));
+
+    carouselInner.addEventListener("mouseover", handleMouseOver);
+    carouselInner.addEventListener("mouseout", handleMouseOut);
+
+    return () => {
+      clearInterval(autoScroll);
+      carouselInner.removeEventListener("mouseover", handleMouseOver);
+      carouselInner.removeEventListener("mouseout", handleMouseOut);
+    };
+  }, [currentIndex]);
+
+  return (
+    <section className="populares">
+      <h1>Populares!</h1>
+      <div className="carousel">
+        <div className="carousel-inner" ref={carouselInnerRef}>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Banana Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Chocolate Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Morango Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Baunilha Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Café Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+          <div className="card">
+            <img src="Imagens/sorvete-de-chocolate.png" alt="" />
+            <h3>Caramelo Soft Cream</h3>
+            <p>R$ 9,99</p>
+          </div>
+        </div>
+        <button className="carousel-prev" onClick={handlePrevClick}>
+          &lt;
+        </button>
+        <button className="carousel-next" onClick={handleNextClick}>
+          &gt;
+        </button>
+      </div>
+      <div className="ver-tudo">
+        <a href="/">ver tudo</a>
+      </div>
+    </section>
+  );
+};
+
+export default CarrosselImagens;
--- senhor-sorvete/src/Pages/Home/index.jsx
+++ senhor-sorvete/src/Pages/Home/index.jsx
@@ -1,9 +1,22 @@
 import "./home.css";
+import React, { useEffect, useState } from "react";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
 import IconeWhatsapp from "../../Components/IconeWhatsapp";
+import CarrosselImagens from "../../Components/CarrosselImagens";
 
 const Home = (props) => {
+  // Função data atual
+  const [dataAtual, setDataAtual] = useState("");
+
+  useEffect(() => {
+    const data = new Date();
+    const dia = String(data.getDate()).padStart(2, "0");
+    const mes = String(data.getMonth() + 1).padStart(2, "0");
+    const ano = data.getFullYear();
+    setDataAtual(`${dia}/${mes}/${ano}`);
+  }, []);
+
   return (
     <div className="home">
       <Header />
@@ -20,7 +33,6 @@ const Home = (props) => {
             </p>
             <div className="buttons">
               <button className="btn-primary">Saiba Mais</button>
-              <button className="btn-secondary">Entrar em contato</button>
             </div>
           </div>
           <img src="Imagens/imagem-principal-homepage.png" alt="" />
@@ -29,13 +41,10 @@ const Home = (props) => {
         <section className="sobre-nos">
           <div className="sobre-container">
             <div className="sobre-img">
-              <img
-                src="Imagens/sorvete-doce-de-leite.png"
-                alt="Imagem sobre nós"
-              />
+              <img src="Imagens/imagem-2-homepage.png" alt="Imagem sobre nós" />
             </div>
             <div className="sobre-text">
-              <h2>——— Sobre nós ———</h2>
+              <h2>Sobre nós</h2>
               <p>
                 Somos apaixonados por oferecer os melhores sorvetes do mercado,
                 selecionando cuidadosamente para revenda em nossa loja. Com um
@@ -51,88 +60,58 @@ const Home = (props) => {
             </div>
           </div>
         </section>
-
-        <section className="populares">
-          <h1>——— Populares! ———</h1>
-          <div className="carousel">
-            <div className="carousel-inner">
-              <div className="card">
-                <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Banana Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-              <div className="card">
-              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Chocolate Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-              <div className="card">
-              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Morango Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-              <div className="card">
-              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Baunilha Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-              <div className="card">
-              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Café Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-              <div className="card">
-              <img src="Imagens/sorvete-de-chocolate.png" alt="" />
-                <h3>Caramelo Soft Cream</h3>
-                <p>R$ 9,99</p>
-              </div>
-            </div>
-            <button className="carousel-prev">&lt;</button>
-            <button className="carousel-next">&gt;</button>
-          </div>
-          <div className="ver-tudo">
-            <a href="/">ver tudo</a>
-          </div>
-        </section>
-
-
+        <CarrosselImagens />
         <section className="sugestao-do-dia">
-        {/* <h2>Sugestão do Dia</h2>
-              <p className="data">
-                <span id="data-atual"></span>
-              </p> */
-              /* <div class name="sugestao-container"> </div> */}
-          <div className="sugestao-container">
-            <div className="sugestao-img">
-              <img src="Imagens/imagem-3-homepage.png" alt="Sugestão do Dia - Chocolate Trufado" />
-            </div>
-            <div className="sugestao-text">
-              <h3 className="titulo-sugestao">Chocolate trufado</h3>
-              <p className="descricao">
-              Hoje, nossa dica especial é o irresistível sorvete de Chocolate Trufado. Com uma combinação perfeita de cremosidade e sabor intenso. Venha experimentar essa delícia que derrete na boca e transforma o seu dia em um momento de puro prazer!
-              </p>
-              <button className="btn-experimentar">Experimentar</button>
+          <h2>SUGESTÃO DO DIA</h2>
+          <p className="data">
+            <span id="data-atual">{dataAtual}</span>
+          </p>
+          <div className="sugestao-do-dia-child">
+            <div className="sugestao-container">
+              <div className="sugestao-img">
+                <img
+                  src="Imagens/imagem-3-homepage.png"
+                  alt="Sugestão do Dia - Chocolate Trufado"
+                />
+              </div>
+              <div className="sugestao-text">
+                <h3 className="titulo-sugestao">Chocolate trufado</h3>
+                <p className="descricao">
+                  Hoje, nossa dica especial é o irresistível sorvete de
+                  Chocolate Trufado. Com uma combinação perfeita de cremosidade
+                  e sabor intenso. Venha experimentar essa delícia que derrete
+                  na boca e transforma o seu dia em um momento de puro prazer!
+                </p>
+                <button className="btn-experimentar">Experimentar</button>
               </div>
             </div>
+          </div>
         </section>
 
         <section className="more-info">
           <div className="more-text">
-            <h1>——— Mais! ———</h1>
+            <h1>Mais!</h1>
             <p>
-            Mergulhe na arte da culinária gastronômica com um toque refrescante! Descubra como os mestres da cozinha transformam ingredientes simples em verdadeiras obras de arte geladas. Acompanhe nossos vídeos inspiradores, onde exploramos técnicas sofisticadas de preparo de sorvetes e picolés, desde a escolha dos ingredientes até a apresentação final.
+              Mergulhe na arte da culinária gastronômica com um toque
+              refrescante! Descubra como os mestres da cozinha transformam
+              ingredientes simples em verdadeiras obras de arte geladas.
+              Acompanhe nossos vídeos inspiradores, onde exploramos técnicas
+              sofisticadas de preparo de sorvetes e picolés, desde a escolha dos
+              ingredientes até a apresentação final.
             </p>
             <button className="btn-primary">Saiba Mais</button>
           </div>
           <div className="more-img">
-            <img src="Imagens/sorvete-chocolate-plantas.png" alt="Imagem placeholder" />
+            <img src="Imagens/imagem-4-homepage.png" alt="Imagem placeholder" />
           </div>
         </section>
 
         <section className="newsletter">
           <h2>Receber notificações</h2>
           <p>
-          Não perca a chance de ser o primeiro a saber quando novos sabores chegam à nossa sorveteria! Inscreva-se para receber notificações por e-mail e fique por dentro de todas as novidades.
+            Não perca a chance de ser o primeiro a saber quando novos sabores
+            chegam à nossa sorveteria! Inscreva-se para receber notificações por
+            e-mail e fique por dentro de todas as novidades.
           </p>
           <form className="newsletter-form">
             <input type="email" placeholder="Email" required />
