e4a31248fcdae5ce03c910c15893d922de30136a
ThathaCampos <thamires.campos@sptech.school>
2024-11-13 15:17:29 -0300

    Iniciando integração da tela de recomendações

--- senhor-sorvete/src/Pages/ADM/Recomendacoes/index.jsx
+++ senhor-sorvete/src/Pages/ADM/Recomendacoes/index.jsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableHead, TableRow, Autocomplete, TextField } from '@mui/material';
 import './recomendacoes.css';
 import TableContainer from '@mui/material/TableContainer';
 import EditIcon from '@mui/icons-material/Edit';
@@ -6,27 +6,66 @@ import { useEffect, useState } from 'react';
 import HeaderGerenciamento from '../../../Components/HeaderGerenciamento';
 import BotaoVoltarGerenciamento from '../../../Components/BotaoVoltarGerenciamento';
 import ModalGerenciamento from '../../../Components/ModalGerenciamento';
+import { toast } from 'react-toastify';
+import axios from "axios";
 
 const Recomendacao = () => {
     const [produtos, setProdutos] = useState([]);
-    const [erro] = useState(null);
+    const [todosProdutos, setTodosProdutos] = useState([]); // Estado para armazenar todos os produtos disponíveis
+    const [erro, setErro] = useState(null);
     const [modalOpen, setModalOpen] = useState(false); 
     const [selectedProduto, setSelectedProduto] = useState(null); 
 
     useEffect(() => {
-        const mockProdutos = [
-            { id: 1, nome: 'Uvinha', marca: 'Bulldogs', preco: 'R$' + 10.0},
-        ];
-        setProdutos(mockProdutos);  
+        // Busca dos produtos recomendados do dia
+        axios.get('http://localhost:8080/produtos/recomendacao-do-dia')
+            .then(response => {
+                console.log("Produtos recomendados:", response.data);  // Verifique o que está sendo retornado
+                const produtosData = Array.isArray(response.data) ? response.data : [];
+                setProdutos(produtosData);
+            })
+            .catch(error => {
+                setErro('Erro ao carregar produtos.');
+                console.error("Erro na requisição:", error);
+            });
+    
+        // Busca de todos os produtos disponíveis para exibir na seleção
+        const token = sessionStorage.getItem('token');
+        axios.get('http://localhost:8080/produtos', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(response => {
+            console.log("Todos os produtos carregados:", response.data);  // Verifique os produtos carregados
+            setTodosProdutos(response.data);
+        })
+        .catch(error => {
+            console.error("Erro ao carregar todos os produtos:", error);
+        });
     }, []);
+    
 
     const handleEditClick = (produto) => {
         setSelectedProduto(produto);
         setModalOpen(true); 
     };
 
-    const handleSave = () => {
-        setModalOpen(false);
+    const atualizarProduto = async () => {
+        try {
+            const token = sessionStorage.getItem('token');
+            await axios.put(`http://localhost:8080/produtos/recomendacao-do-dia/${selectedProduto.id}`, selectedProduto, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            toast.success('Produto atualizado com sucesso!');
+            setModalOpen(false);
+            setProdutos(produtos.map(prod => prod.id === selectedProduto.id ? selectedProduto : prod));
+        } catch (error) {
+            toast.error('Erro ao atualizar o produto.');
+            console.error("Erro ao atualizar:", error);
+        }
     };
 
     const handleCloseModal = () => {
@@ -48,7 +87,7 @@ const Recomendacao = () => {
                     <p style={{ color: 'red' }}>{erro}</p>
                 ) : (
                     <TableContainer component={Paper} className='container-tabela'>
-                        <Table arial-label="Tabela">
+                        <Table aria-label="Tabela">
                             <TableHead className='tabela-Head'>
                                 <TableRow>
                                     <TableCell className='tabela-head'>Nome</TableCell>
@@ -58,16 +97,22 @@ const Recomendacao = () => {
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {produtos.map(produto => (
-                                    <TableRow key={produto.id} className='tabela-row'>
-                                        <TableCell className='tabela-cell'>{produto.nome}</TableCell>
-                                        <TableCell className='tabela-cell'>{produto.marca}</TableCell>
-                                        <TableCell className='tabela-cell'>{produto.preco}</TableCell>
-                                        <TableCell className='tabela-cell'>
-                                            <button onClick={() => handleEditClick(produto)}><EditIcon /></button>
-                                        </TableCell>
+                                {produtos.length > 0 ? (
+                                    produtos.map(produto => (
+                                        <TableRow key={produto.id} className='tabela-row'>
+                                            <TableCell className='tabela-cell'>{produto.nome}</TableCell>
+                                            <TableCell className='tabela-cell'>{produto.marca?.nome || 'N/A'}</TableCell>
+                                            <TableCell className='tabela-cell'>{produto.preco}</TableCell>
+                                            <TableCell className='tabela-cell'>
+                                                <button onClick={() => handleEditClick(produto)}><EditIcon /></button>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={4} style={{ textAlign: 'center' }}>Nenhum produto disponível.</TableCell>
                                     </TableRow>
-                                ))}
+                                )}
                             </TableBody>
                         </Table>
                     </TableContainer>
@@ -78,13 +123,25 @@ const Recomendacao = () => {
                 <ModalGerenciamento
                     open={modalOpen}
                     onClose={handleCloseModal}
-                    title={`Editar ${selectedProduto.nome}`}
+                    title="Selecionar Novo Produto"
                     fields={[
-                        { label: 'Nome do Produto', type: 'text', defaultValue: selectedProduto.nome },
-                        { label: 'Marca', type: 'text', defaultValue: selectedProduto.marca },
-                        { label: 'Preço', type: 'number', defaultValue: selectedProduto.preco }
+                        {
+                            label: 'Selecione um novo produto',
+                            component: (
+                                <Autocomplete
+                                    options={todosProdutos}
+                                    getOptionLabel={(option) => option.nome}
+                                    onChange={(event, newValue) => {
+                                        console.log("Produto selecionado:", newValue);  // Depuração
+                                        setSelectedProduto(newValue);
+                                    }}
+                                    renderInput={(params) => <TextField {...params} label="Produto" />}
+                                    value={selectedProduto || null}
+                                />
+                            ),
+                        }
                     ]}
-                    onSave={handleSave}
+                    onSave={atualizarProduto}
                 />
             )}
         </>
