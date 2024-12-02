1c4cf1bfb129382e10f312aac62968441c1a9547
gui-oliveiraa <guilherme.oliveira@sptech.school>
2024-12-02 15:44:09 -0300

    Finalizando responsividade da tela de gastronomia

--- senhor-sorvete/src/Pages/Gastronomia/index.jsx
+++ senhor-sorvete/src/Pages/Gastronomia/index.jsx
@@ -21,21 +21,24 @@ const Gastronomia = (props) => {
           imagem="/Imagens/casquinhas-de-chocolate.jpeg"
           placeholder="Imagem com dois sorvetes de casquinhas de chocolate"
           titulo="MELHORES SABORES"
-          texto="Sorvetes em uma incrível variedade de sabores encantam todos os paladares: desde os clássicos e atemporais como baunilha, chocolate e morango, até opções mais sofisticadas, como pistache e tiramisu, que oferecem uma experiência mais refinada. Para quem busca frescor, sabores frutados como manga e limão siciliano são uma explosão de frescor e leveza."
+          texto="Sorvetes em uma incrível variedade de sabores encantam todos os paladares: desde os clássicos e atemporais como baunilha, chocolate e morango, até opções mais sofisticadas, como pistache e tiramisu, que oferecem uma experiência mais refinada."
+          link="https://areademulher.r7.com/curiosidades/o-que-o-seu-sorvete-favorito-diz-sobre-sua-personalidade/"
         />
+
         <Artigo
           imagem="/Imagens/sorvete-e-rosto.jpeg"
           placeholder="Imagem sobre a história do sorvete"
           titulo="PRIMEIRO SORVETE CRIADO"
           texto="O primeiro sabor de sorvete, uma mistura primitiva e refrescante, foi criado pelos chineses há cerca de 3.000 anos. Essa iguaria consistia em uma combinação simples, mas inovadora, de neve ou gelo triturado, misturado com frutas e mel, proporcionando uma sobremesa gelada que trazia alívio e prazer nos climas quentes."
+          link="https://www.perfetto.com.br/blog/voce-sabe-como-surgiu-o-sorvete-conheca-a-historia-dessa-iguaria/"
         />
       </div>
 
       <TextoVideo
-        Style="flex-direction: row-reverse"
         titulo="Dicas para sua sobremesa gelada!"
         texto="O sorvete é uma sobremesa versátil que combina bem com brownies, petit gâteau, waffles e profiteroles, criando um contraste delicioso entre quente e frio. Entre as frutas, morangos, bananas, mangas e frutas vermelhas são ótimas escolhas, assim como frutas flambadas, que contrastam com a cremosidade do sorvete."
         video="https://www.youtube-nocookie.com/embed/l1RHDbMaUCI?si=lWdw1uMy2nedHx4r"
+        isSecondInstance={true}
       />
 
       <Footer />
